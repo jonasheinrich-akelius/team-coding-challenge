4523d8e0ce4d5a6af63aa9c3829c385dd5094c0f
sandrawendrich <sandra.wendrich@akelius.de>
2022-01-07 14:26:24 +0100

    Step3 + Step4

--- diamonds/main.js
+++ diamonds/main.js
@@ -1,7 +1,14 @@
 makeDiamond = (char) => {
     const startCharacter = 'A'.charCodeAt(0);
     const midRowCharacter = char.charCodeAt(0);
+		let diamond = "";
+
     for(let i = startCharacter; i <= midRowCharacter; i++) {
-        console.log(String.fromCharCode(i));
+			if (startCharacter !== i) {
+				diamond += String.fromCharCode(i);
+			}
+			diamond += String.fromCharCode(i);
     }
+
+		console.log(diamond);
 }
