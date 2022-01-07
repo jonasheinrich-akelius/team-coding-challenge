cdf1f00e4160aa156c282175be4673f7504c67a3
Rene Eichhorn <40768172+rene-eichhorn@users.noreply.github.com>
2022-01-07 14:10:59 +0100

    implement loop

--- diamonds/main.js
+++ diamonds/main.js
@@ -1,3 +1,7 @@
 makeDiamond = (char) => {
-  console.log(char);
+    const startCharacter = 'A'.charCodeAt(0);
+    const midRowCharacter = char.charCodeAt(0);
+    for(let i = startCharacter; i <= midRowCharacter; i++) {
+        console.log(String.fromCharCode(i));
+    }
 }
