d5a7392441008ddab35ea3134a2b44a5b1d74af3
Jonas Heinrich <jonas.heinrich@akelius.de>
2022-01-14 14:29:37 +0100

    Format

--- diamonds/main.js
+++ diamonds/main.js
@@ -1,31 +1,31 @@
 reverseString = (str) => {
   const array = str.split("");
   return array.reverse().join("");
-}
+};
 
 getDots = (currentRow, midRow) => {
   return ".".repeat(midRow - currentRow);
-}
+};
 
 makeDiamond = (char) => {
-    const startCharacter = 'A'.charCodeAt(0);
-    const midRowCharacter = char.charCodeAt(0);
-		let diamondTop = "";
+  const startCharacter = "A".charCodeAt(0);
+  const midRowCharacter = char.charCodeAt(0);
+  let diamondTop = "";
 
-    for(let i = startCharacter; i < midRowCharacter; i++) {
-			diamondTop += getDots(i, midRowCharacter);
-      if (startCharacter !== i) {
-				diamondTop += String.fromCharCode(i);
-			}
-			diamondTop += String.fromCharCode(i);
-      diamondTop += getDots(i, midRowCharacter);
-      diamondTop += "\n";
+  for (let i = startCharacter; i < midRowCharacter; i++) {
+    diamondTop += getDots(i, midRowCharacter);
+    if (startCharacter !== i) {
+      diamondTop += String.fromCharCode(i);
     }
+    diamondTop += String.fromCharCode(i);
+    diamondTop += getDots(i, midRowCharacter);
+    diamondTop += "\n";
+  }
 
-    const diamondBottom = reverseString(diamondTop);
+  const diamondBottom = reverseString(diamondTop);
 
-    const diamondMiddle = char + char;
-    const diamond = diamondTop + diamondMiddle + diamondBottom;
+  const diamondMiddle = char + char;
+  const diamond = diamondTop + diamondMiddle + diamondBottom;
 
-    console.log(diamond);
-}
+  console.log(diamond);
+};
