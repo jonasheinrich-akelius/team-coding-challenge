1e6995edf9739c28a17307a328af7fc8095fd68a
Mohamed Ibrahim <61151838+mohamedibra91@users.noreply.github.com>
2022-01-21 14:22:47 +0100

    store blocks

--- abc-building-blocks/main.js
+++ abc-building-blocks/main.js
@@ -1,6 +1,20 @@
 
+getBlock = (letters) => {
+   return {
+      string: letters,
+      isUsed: false
+    }
+}
+
+var blocks = [
+   getBlock("BO"),
+   getBlock("XK"),
+   getBlock("DQ"),
+   getBlock("CP"),
+];
 
 createWord = (word) => {
-  console.log(word)
-
+  console.log(word);
+  console.log(blocks);
 }
+
