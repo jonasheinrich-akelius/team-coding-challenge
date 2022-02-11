6df85e4cd76af51de30e8bcef206952967dacb98
Rene Eichhorn <40768172+rene-eichhorn@users.noreply.github.com>
2022-02-11 14:45:48 +0100

    clean up and get first number of number with multiple digits

--- combined-numbers/main.js
+++ combined-numbers/main.js
@@ -1,22 +1,38 @@
+hasOnlyPositiveNumbers = (numbers) => {
+    let hasNegativeNumber = false;
+    let i = 0;
+	while(i< numbers.length) {
+		i++;   
+		const number = numbers[i];
+		if(number < 0) {
+            console.log(number,"should be positive!");
+            hasNegativeNumber = true;
+            break;
+		}
+	}
+    return !hasNegativeNumber;
+}
+
 largestNumberOfArray = (numbers) => {
-	return Math.max.apply(null, numbers)
+	return Math.max(...numbers);
+}
+
+largestStartingNumberOfArray = (numbers) => {
+    for(let i = 0; i < numbers.length; i++) {
+        let number = numbers[i].toString();
+        if(number.length > 1) {
+            console.log("first number of", number, "is", number.charAt(0));
+        } else {
+            console.log(number,"has only one digit");
+        }
+    }
 }
 
 combinedLargerstNumber = (arrayOfNumbers) => {
-	let i = 0;
-	while(i< arrayOfNumbers.length) {
-		i++;   
-		const number = arrayOfNumbers[i];
-		if(number < 0) {
-				console.log(number,"should be positive!");
-				break;
-		}
-	}
-
-	largestNumberOfArray(arrayOfNumbers);
-	console.log(largestNumberOfArray(arrayOfNumbers));
-	console.log(arrayOfNumbers)
+	if(hasOnlyPositiveNumbers(arrayOfNumbers)) {
+        console.log(largestNumberOfArray(arrayOfNumbers));
+        largestStartingNumberOfArray(arrayOfNumbers);
+        console.log(arrayOfNumbers)
+    }
 }
-
-
 
