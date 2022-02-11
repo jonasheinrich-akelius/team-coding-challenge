59f8d9ab7378151418ee714c73856033a4cc189d
sandrawendrich <sandra.wendrich@akelius.de>
2022-02-11 14:24:10 +0100

    add largestNumberOfArray

--- combined-numbers/main.js
+++ combined-numbers/main.js
@@ -1,16 +1,22 @@
+largestNumberOfArray = (numbers) => {
+	return Math.max.apply(null, numbers)
+}
+
 combinedLargerstNumber = (arrayOfNumbers) => {
-let i = 0;
-while(i< arrayOfNumbers.length) {
-   i++;   
-   const number = arrayOfNumbers[i];
-   if(number < 0) {
-      console.log(number,"should be positive!");
-      break;
-   } else {
+	let i = 0;
+	while(i< arrayOfNumbers.length) {
+		i++;   
+		const number = arrayOfNumbers[i];
+		if(number < 0) {
+				console.log(number,"should be positive!");
+				break;
+		}
+	}
 
-   }
+	largestNumberOfArray(arrayOfNumbers);
+	console.log(largestNumberOfArray(arrayOfNumbers));
+	console.log(arrayOfNumbers)
 }
 
-console.log(arrayOfNumbers)
 
-}
+
