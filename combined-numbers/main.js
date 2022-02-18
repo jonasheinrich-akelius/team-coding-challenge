b3239f1f14cfb580bced8fd10071edd3f7ddad2e
Rene Eichhorn <40768172+rene-eichhorn@users.noreply.github.com>
2022-02-18 15:30:20 +0100

    refactor again and idea for next time

--- combined-numbers/main.js
+++ combined-numbers/main.js
@@ -11,13 +11,21 @@ hasNegativeNumbers = (numbers) => {
     return false;
 }
 
-compareFirstDigit = (numberA, numberB) => {
-  const firstDigitAAsString = String(numberA)[0];
-  const firstDigitBAsString = String(numberB)[0];
-  const firstDigitA = Number(firstDigitAAsString);
-  const firstDigitB = Number(firstDigitBAsString);
+compareDigitAtIndex = (numberA, numberB, index) => {
+  const digitAAsString = String(numberA)[index];
+  const digitBAsString = String(numberB)[index];
+  const digitA = Number(digitAAsString);
+  const digitB = Number(digitBAsString);
 
-  return firstDigitA > firstDigitB ? -1 : 1;
+  return digitA > digitB ? -1 : 1;
+}
+
+compareNumbers = (numberA, numberB) => {
+    return numberA > numberB ? -1 : 1;
+}
+
+getSortedArray = (numbers) => {
+    return numbers.sort(compareNumbers);
 }
 
 combinedLargerstNumber = (arrayOfNumbers) => {
@@ -25,7 +33,13 @@ combinedLargerstNumber = (arrayOfNumbers) => {
         return;
     }
 
-    const sortedArray = arrayOfNumbers.sort(compareFirstDigit);
+    // idea for the next time
+    // first group the numbers by their first digit
+    // [42, 426, 5, 55, 51, 2, 22, 43, 9, 6] would be grouped to [5, 55, 51], [42, 426, 43], [2, 22], [9], [6]
+    // then sort every grouped array
+    // join the grouped and sorted arrays together desc based on their first digit
+
+    const sortedArray = getSortedArray(arrayOfNumbers)
     const joinedArray = sortedArray.join('');
     console.log(joinedArray)
     console.log(sortedArray)
