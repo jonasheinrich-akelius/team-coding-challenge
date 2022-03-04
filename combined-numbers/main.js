ab9815d396389a9dc579cada59f735cf55bf23b3
Rene Eichhorn <40768172+rene-eichhorn@users.noreply.github.com>
2022-03-04 14:02:45 +0100

    add check for comparing if numbers include themselfs

--- combined-numbers/main.js
+++ combined-numbers/main.js
@@ -1,26 +1,34 @@
 hasNegativeNumbers = (numbers) => {
     let i = 0;
-	while(i < numbers.length) { 
-		const number = numbers[i];
-		if(number < 0) {
-            console.log(number,"should be positive!");
+    while (i < numbers.length) {
+        const number = numbers[i];
+        if (number < 0) {
+            console.log(number, "should be positive!");
             return true;
-		}
+        }
         i++;
-	}
+    }
     return false;
 }
 
 compareDigitAtIndex = (numberA, numberB, index) => {
-  const digitAAsString = String(numberA)[index];
-  const digitBAsString = String(numberB)[index];
-  const digitA = Number(digitAAsString);
-  const digitB = Number(digitBAsString);
+    const digitAAsString = String(numberA)[index];
+    const digitBAsString = String(numberB)[index];
+    const digitA = Number(digitAAsString);
+    const digitB = Number(digitBAsString);
 
-  return digitA > digitB ? -1 : 1;
+    return digitA > digitB ? -1 : 1;
+}
+
+containsEachOther = (numberA, numberB) => {
+    return String(numberA).startsWith(String(numberB)) ||
+        String(numberB).startsWith(String(numberA))
 }
 
 compareNumbers = (numberA, numberB) => {
+    if (containsEachOther(numberA, numberB)) {
+        
+    }
     return numberA > numberB ? -1 : 1;
 }
 
@@ -29,15 +37,9 @@ getSortedArray = (numbers) => {
 }
 
 combinedLargerstNumber = (arrayOfNumbers) => {
-	if(hasNegativeNumbers(arrayOfNumbers)) {
+    if (hasNegativeNumbers(arrayOfNumbers)) {
         return;
     }
-
-    // idea for the next time
-    // first group the numbers by their first digit
-    // [42, 426, 5, 55, 51, 2, 22, 43, 9, 6] would be grouped to [5, 55, 51], [42, 426, 43], [2, 22], [9], [6]
-    // then sort every grouped array
-    // join the grouped and sorted arrays together desc based on their first digit
 
     const sortedArray = getSortedArray(arrayOfNumbers)
     const joinedArray = sortedArray.join('');
