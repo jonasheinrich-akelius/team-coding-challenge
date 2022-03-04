7a768d82c39bd8addc584597a1f946472ba22554
Jonas Heinrich <jonas.heinrich@akelius.de>
2022-03-04 14:49:02 +0100

    Fix some bugs ;)

--- combined-numbers/main.js
+++ combined-numbers/main.js
@@ -1,57 +1,71 @@
 hasNegativeNumbers = (numbers) => {
-    let i = 0;
-    while (i < numbers.length) {
-        const number = numbers[i];
-        if (number < 0) {
-            console.log(number, "should be positive!");
-            return true;
-        }
-        i++;
+  let i = 0;
+  while (i < numbers.length) {
+    const number = numbers[i];
+    if (number < 0) {
+      console.log(number, "should be positive!");
+      return true;
     }
-    return false;
-}
+    i++;
+  }
+  return false;
+};
 
 compareDigitAtIndex = (numberA, numberB, index) => {
-    const digitAAsString = String(numberA)[index];
-    const digitBAsString = String(numberB)[index];
-    const digitA = Number(digitAAsString);
-    const digitB = Number(digitBAsString);
+  const digitAAsString = String(numberA)[index];
+  const digitBAsString = String(numberB)[index];
+  const digitA = Number(digitAAsString);
+  const digitB = Number(digitBAsString);
 
-    return digitA > digitB ? -1 : 1;
-}
+  return digitA > digitB ? -1 : 1;
+};
 
 containsEachOther = (numberA, numberB) => {
-    return String(numberA).startsWith(String(numberB)) ||
-        String(numberB).startsWith(String(numberA))
-}
+  return (
+    String(numberA).startsWith(String(numberB)) ||
+    String(numberB).startsWith(String(numberA))
+  );
+};
 
 compareNumbers = (numberA, numberB) => {
-    if (containsEachOther(numberA, numberB)) {
-        console.log('contains', numberA, numberB)
-        if(numberA < numberB){
-           const tailOfLargeNumber = String(numberB).slice(numberA.length);
-           return tailOfLargeNumber < numberA ? -1 : 1;       
-        } else if(numberB < numberA) {
-            const tailOfLargeNumber = String(numberA).slice(numberB.length);
-            return tailOfLargeNumber < numberB ? -1 : 1;   
-        }
-      
+  if (containsEachOther(numberA, numberB)) {
+    console.log("contains", numberA, numberB);
+    if (numberA < numberB) {
+      const tailOfLargeNumber = String(numberB).slice(String(numberA).length);
+      console.log(
+        "A",
+        tailOfLargeNumber,
+        numberA,
+        numberB,
+        tailOfLargeNumber < String(numberA)
+      );
+      return tailOfLargeNumber < String(numberA) ? -1 : 1;
+    } else if (numberB < numberA) {
+      const tailOfLargeNumber = String(numberA).slice(String(numberB).length);
+      console.log(
+        "B",
+        tailOfLargeNumber,
+        numberA,
+        numberB,
+        tailOfLargeNumber < String(numberA)
+      );
+      return tailOfLargeNumber > String(numberB) ? -1 : 1;
     }
-    return numberA > numberB ? -1 : 1;
-}
+  }
+  return String(numberA) > String(numberB) ? -1 : 1;
+};
 
 getSortedArray = (numbers) => {
-    return numbers.sort(compareNumbers);
-}
+  return numbers.sort(compareNumbers);
+};
 
 combinedLargerstNumber = (arrayOfNumbers) => {
-    if (hasNegativeNumbers(arrayOfNumbers)) {
-        return;
-    }
+  if (hasNegativeNumbers(arrayOfNumbers)) {
+    return;
+  }
 
-    const sortedArray = getSortedArray(arrayOfNumbers)
-    const joinedArray = sortedArray.join('');
-    console.log("joinedArray",joinedArray)
-    console.log(sortedArray)
-}
-
+  const sortedArray = getSortedArray(arrayOfNumbers);
+  const joinedArray = sortedArray.join("");
+  console.log("joinedArray", joinedArray);
+  console.log(sortedArray);
+};
