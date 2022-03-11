062cc8e19ce2b389dcab42f103f7f3755c02352f
Rene Eichhorn <40768172+rene-eichhorn@users.noreply.github.com>
2022-03-11 14:33:11 +0100

    first starting point

--- anagrams/main.js
+++ anagrams/main.js
@@ -1,4 +1,12 @@
-anagram = (input) => {
-  console.log(input);
+permutate = (remainingLetters) => {
+    console.log(remainingLetters);
+}
 
+anagram = (input) => {
+    const inputArray = input.split("");
+    for(let i = 0; i < input.length; i++) {
+        const firstLetter = inputArray[i];
+        const remainingLetters = inputArray.splice(i, 1);
+        permutate(remainingLetters);
+    }
 };
