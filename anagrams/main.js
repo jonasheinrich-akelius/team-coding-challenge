87410f217e92410190e0d183643db31d2c8024ff
sandrawendrich <sandra.wendrich@akelius.de>
2022-03-25 14:38:42 +0100

    switch to permutate function

--- anagrams/main.js
+++ anagrams/main.js
@@ -1,16 +1,26 @@
-permutate = (leftchars, remainingLetters) => {
-    if(remainingLetters.length > 0){
-      const joinStrings = leftchars.concat(remainingLetters);
-      console.log("concat string", joinStrings.join(""))
-    }
-}
+// permutate = (mainchar) => {
+//     if(rightchars.length > 0){
+//       const joinStrings = leftchars.concat(rightchars);
+//       console.log("concat string", joinStrings.join(""))
+//     }
+// }
 
-anagram = (input) => {
+permutate = (input) => {
     const inputArray = input.split("");
+		if (inputArray.length === 0) {
+			return [];
+		} 
+
+		if (inputArray.length === 1) {
+			return inputArray;
+		}
+
     for(let i = 0; i < input.length; i++) {
-        const remainingLetters = inputArray.splice(i, 1);
-        const leftchars= inputArray.splice(-i)
-        permutate(leftchars, remainingLetters);
+				const mainchar = inputArray[i];
+        // const rightchars = inputArray.splice(i, 1);
+        // const leftchars = inputArray.splice(-i);
+				// const remainingChars = [leftchars, rightchars]
+        permutate(mainchar);
     }
-    console.log(input)
+    console.log(input);
 };
